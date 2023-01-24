9f0fc327380ea7a37c5d9a5b29f5debe02c788ca
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-01-23 19:30:18 -0500

    merge sorted array

--- MergeSortedArray.js
+++ MergeSortedArray.js
@@ -3,9 +3,9 @@
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @return {void} 
  */
 var merge = function (nums1, m, nums2, n) {
-  nums1.splice(m, nums1.length, ...nums2);
+  nums1.splice(m, nums1.length, ...nums2)
   nums1 = nums1.sort((a, b) => a - b);
 };
